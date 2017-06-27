bfd8c34f9feedf1e584c517f11a28e9227ef2411
msjxy <1012572101@qq.com>
2017-06-27 08:59:14 +0800

    完成框架 歌手界面

---
+++ src/baes/onea/onea.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>拍的发多少</h1>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  h1{
+    color red
+  }
+</style>
--- src/components/two/geshou.vue
+++ src/components/two/geshou.vue
@@ -1,13 +1,18 @@
 <template>
-  <div>
+  <div class="singer" ref="singer">
+    <list-view :data="singelist"></list-view>
   </div>
 </template>
+
 <script type="text/ecmascript-6">
   import {gettwo} from 'api/geshou-data'
   import {ERR_OK} from 'api/config'
   import Singer from 'common/js/singer'
+  import ListView from 'base/gslist/gelist'
+
   const HOT_NAME = '热门'
   const HOT_NAME_LENGTH = 10
+
   export default {
     data() {
       return {
@@ -21,9 +26,8 @@
       geshoulist() {
         gettwo().then((res) => {
           if (res.code === ERR_OK) {
-            this.singelist = res.data.list
+            this.singelist = this.singderlist(res.data.list)
           }
-          console.log(this.singderlist(this.singelist))
         })
       },
       singderlist(list) {
@@ -52,7 +56,6 @@
             name: item.Fsinger_name
           }))
         })
-        console.log(map)
 //        为了得到有序列表 处理map
         let hot = []
         let ret = []
@@ -67,13 +70,20 @@
         ret.sort((a, b) => {
           return a.title.charCodeAt(0) - b.title.charCodeAt(0)
         })
-        console.log(ret)
         return hot.concat(ret)
       }
+    },
+    components: {
+      ListView
     }
   }
+
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
